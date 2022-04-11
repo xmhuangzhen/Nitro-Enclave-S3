c1f3b59b62598d861a4315ffb5daf98eb3d6d158
xmhuangzhen <xmhuangzhen@sjtu.edu.cn>
2022-04-11 14:42:26 +0800

    run sh upd

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+amazon-linux-extras install aws-nitro-enclaves-cli -y
+yum install aws-nitro-enclaves-cli-devel -y
+yum install amazon-ecr-credential-helper -y
+yum install python3 -y
+
+
 su ec2-user -c 'aws s3 cp s3://kmg-ps-public/enclave.eif $HOME/enclave.eif'
 
 count=1
